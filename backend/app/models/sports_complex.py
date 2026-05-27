4448fc2a9c91d9aeb99038308fa29433abec5c6e
devupc73 <45304649+devupc73@users.noreply.github.com>
2026-05-27 01:39:14 -0500

    Add sports complex model

---
+++ backend/app/models/sports_complex.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column
+from sqlalchemy import Float
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from app.database import Base
+
+
+class SportsComplex(Base):
+    __tablename__ = 'sports_complexes'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    status = Column(String, default='active')
